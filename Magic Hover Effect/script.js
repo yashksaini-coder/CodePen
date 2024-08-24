68e1c871bc338993f0d8fb440c10f259febe4161
yashksaini-coder <ys3853428@gmail.com>
2024-08-24 23:42:54 +0530

    Magic Hover Effect complete

---
+++ Magic Hover Effect/script.js
@@ -0,0 +1,10 @@
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
